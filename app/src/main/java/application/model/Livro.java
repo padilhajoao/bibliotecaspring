d6a9799e2d7f075fdab4c78dbf20b263b72f8f95
marcoweb <marcocarvalho.web@gmail.com>
2023-05-02 18:30:09 +0000

    Modelo

--- app/src/main/java/application/model/Livro.java
+++ app/src/main/java/application/model/Livro.java
@@ -1,11 +1,16 @@
 package application.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="livros")
 public class Livro {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
     private String titulo;
 
